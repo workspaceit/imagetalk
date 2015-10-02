bbc849d1e8b86fce20255624928467c72d88981f
mi_rafi <rafi@workspaceit.com>
2015-10-02 12:44:28 +0600

    Minor change

--- src/model/datamodel/operational/OperAppCredential.java
+++ src/model/datamodel/operational/OperAppCredential.java
@@ -6,8 +6,10 @@ import model.datamodel.app.AppCredential;
  * Created by mi on 10/2/15.
  */
 public class OperAppCredential extends AppCredential {
+    boolean active;
     boolean banned;
     public OperAppCredential(){
+        this.active = false;
         this.banned = false;
     }
 }
