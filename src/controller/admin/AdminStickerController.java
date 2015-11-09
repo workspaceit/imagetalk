e8b9574ba8f7084c7e9f67afd7dbf91f9b60bb3b
Md.Rajib Hossen <rajib@workspaceit.com>
2015-11-09 15:14:10 +0600

    sticker usage eligible added

--- src/controller/admin/AdminStickerController.java
+++ src/controller/admin/AdminStickerController.java
@@ -22,13 +22,13 @@ import java.util.ArrayList;
  * Created Date     : 10/9/15
  */
 
-public class StickerController extends HttpServlet {
+public class AdminStickerController extends HttpServlet {
     private PrintWriter             pw;
     private String                  url;
     private Login                   login;
     private ImageTalkBaseController baseController;
 
-    public StickerController() {
+    public AdminStickerController() {
         this.login = new Login();
         this.baseController = new ImageTalkBaseController();
     }
