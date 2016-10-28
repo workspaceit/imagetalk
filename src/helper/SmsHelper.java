9df7d5533f1425f0e4732212d7b479dfb5b42d04
mi_rafi <rafi@workspaceit.com>
2016-10-28 14:03:51 +0600

    Sms API UPDATE

--- src/helper/SmsHelper.java
+++ src/helper/SmsHelper.java
@@ -1,36 +1,60 @@
 package helper;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Created by mi on 10/26/16.
  */
 public class SmsHelper {
-    private static  final  String userName="wsit_developer";
-    private static  final  String password="GKeGPVedBecTgb";
-    private static  final  String apiId = "3630432";
-    private static  final  String url = "http://api.clickatell.com/http/sendmsg";
+    private static  final  String userName="AC19bb7644b44dec9d088a8dd07d124f6e";
+    private static  final  String password="2756f69b55b7f3a6d044afbcb5aa8e6f";
+    private static  final  String url = "https://api.twilio.com/2010-04-01/Accounts/AC19bb7644b44dec9d088a8dd07d124f6e/Messages";
 
     public static void sendRegistrationCode(String to, String code ) throws IOException {
-    //api.clickatell.com/http/sendmsg?user=wsit_developer&password=GKeGPVedBecTgb&api_id=3630432&to=8801723810272&text=Message
 
+        if(!to.contains("+")){
+            to = "+"+to;
+        }
         String msg = "Your ImageTalk confirmation token : "+code;
-        URL obj = new URL(url+"?user="+userName+"&password="+password+"&api_id="+apiId+"&to="+to+"&text="+msg);
+        URL obj = new URL(url);
+        String encoded = Base64.getEncoder().encodeToString((userName + ":" + password).getBytes(StandardCharsets.UTF_8));
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestProperty("Authorization", "Basic "+encoded);
+        StringBuilder postData = new StringBuilder();
+        Map<String,Object> params = new LinkedHashMap<>();
+
+        params.put("To",to);
+        params.put("From","+18559798993");
+        params.put("Body",msg);
+
+        for (Map.Entry<String,Object> param : params.entrySet()) {
+            if (postData.length() != 0) postData.append('&');
+            postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+            postData.append('=');
+            postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+        }
+        byte[] postDataBytes = postData.toString().getBytes("UTF-8");
 
         // optional default is GET
-        con.setRequestMethod("GET");
-
+        con.setRequestMethod("POST");
+        con.setDoOutput(true);
+        con.getOutputStream().write(postDataBytes);
         //add request header
        // con.setRequestProperty("User-Agent", USER_AGENT);
 
         int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
+        System.out.println("Sending 'GET' request to URL : " + url);
         System.out.println("Response Code : " + responseCode);
 
         BufferedReader in = new BufferedReader(
@@ -49,7 +73,7 @@ public class SmsHelper {
     }
     public static void main(String args[]){
         try {
-            SmsHelper.sendRegistrationCode("8801670396449","4567");
+            SmsHelper.sendRegistrationCode("toNUmber","Code");
         } catch (IOException e) {
             e.printStackTrace();
         }
